d5da89ed26567ea4544055c17aaaca005061b812
MasonBeale <mbeale@cedarville.edu>
2026-04-19 15:08:29 -0400

    Add schema and PJO for events

--- src/main/java/mason/beale/achievements/Consumers/PlayerActionEventConsumer.java
+++ src/main/java/mason/beale/achievements/Consumers/PlayerActionEventConsumer.java
@@ -2,12 +2,14 @@ package mason.beale.achievements.Consumers;
 
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+import mason.beale.achievements.events.PlayerActionEvent;
+import jakarta.validation.Valid;
 
 @Service
 public class PlayerActionEventConsumer {
     
     @KafkaListener(topics = "player-actions", groupId = "my-consumer-group")
-    public void consume(String message) {
+    public void consume(@Valid PlayerActionEvent message) {
         System.out.println("Received message: " + message);
     }
 }
---
+++ src/main/java/mason/beale/achievements/events/PlayerActionEvent.java
@@ -0,0 +1,77 @@
+package mason.beale.achievements.events;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
+
+public class PlayerActionEvent {
+    
+    @NotNull(message = "playerId is required")
+    @JsonProperty("playerId")
+    private int playerId;
+    
+    @NotNull(message = "action is required")
+    @JsonProperty("action")
+    private String action;
+    
+    @NotNull(message = "points is required")
+    @JsonProperty("points")
+    private int points;
+    
+    @NotNull(message = "timestamp is required")
+    @JsonProperty("timestamp")
+    private long timestamp;
+    
+    // Constructors
+    public PlayerActionEvent() {
+    }
+    
+    public PlayerActionEvent(int playerId, String action, int points, long timestamp) {
+        this.playerId = playerId;
+        this.action = action;
+        this.points = points;
+        this.timestamp = timestamp;
+    }
+    
+    // Getters and Setters
+    public int getPlayerId() {
+        return playerId;
+    }
+    
+    public void setPlayerId(int playerId) {
+        this.playerId = playerId;
+    }
+    
+    public String getAction() {
+        return action;
+    }
+    
+    public void setAction(String action) {
+        this.action = action;
+    }
+    
+    public int getPoints() {
+        return points;
+    }
+    
+    public void setPoints(int points) {
+        this.points = points;
+    }
+    
+    public long getTimestamp() {
+        return timestamp;
+    }
+    
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+    
+    @Override
+    public String toString() {
+        return "PlayerActionEvent{" +
+                "playerId=" + playerId +
+                ", action='" + action + '\'' +
+                ", points=" + points +
+                ", timestamp=" + timestamp +
+                '}';
+    }
+}
